e7f86d8c0d6a3c8b16d686efec0c8cedf249b8b5
fidaatag <fidaa2fidaa@gmail.com>
2023-02-24 08:27:57 +0700

    conditional-rendering if

--- src/app.js
+++ src/app.js
@@ -1,43 +1,24 @@
 const root = document.querySelector('#root');
 
-console.log('Automatic compile!')
+function App() {
+    const [login, setLogin] = React.useState(false);
 
-function tick() {
-    /** Render ala react
-     *  hanyak akan merender element yg memang butuh berubah seperti jam
-     *  lebih efisien dan menghemat memori
-     **/
-    const element = (
-        <div>
-            <h1>Sekarang Jam</h1>
-            <h2>{new Date().toLocaleTimeString()}</h2>
-        </div>
-    );
+    if(login) {
+        return (
+            <>
+            <h1>Yea! Bisa login</h1>;
+            <button onClick ={function() {setLogin(false)}}>Log Out</button>;
+            </>
+        );
+    }
 
-    // render untuk memunculkan element
-    ReactDOM.render(element, root);
-
-    
-    /** Template literal - cara vanilla js
-     *  semua element yg ada didalam div dirender 
-     *  namun, untuk bisa mengatur agar hanya jam saja yang terupdate cukup sulit
-     *  cara ini tidak efiesien, 
-     *  terutama saat terdapat eventhandler, dan juga
-     *  membuat memori lebih banyak digunakan
-     * 
-     
-    const element = `
-        <div>
-            <h1>Sekarang Jam</h1>
-            ${new Date().toLocaleTimeString()}
-        </div>
-    `;
-
-    // render untuk memunculkan element
-    root.innerHTML = element **/
+    return (
+        <>
+            <h1>Login Dulu, Sayton!</h1>;
+            <button onClick ={function() {setLogin(true)}}>Log In</button>;
+        </>
+    )
 }
 
-tick();
-setInterval(function() {
-    tick();
-}, 1000);
+// render untuk memunculkan element
+ReactDOM.render(<App/>, root);
