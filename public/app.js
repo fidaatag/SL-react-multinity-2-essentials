f800fadef8079af176dfdcf93d2abb424efa45db
fidaatag <fidaa2fidaa@gmail.com>
2023-02-24 11:11:59 +0700

    form get value uncontroled componen

--- public/app.js
+++ public/app.js
@@ -1,34 +1,21 @@
 const root = document.querySelector('#root');
-console.log('Automatic compile!');
-function tick() {
-  /** Render ala react
-   *  hanyak akan merender element yg memang butuh berubah seperti jam
-   *  lebih efisien dan menghemat memori
-   **/
-  const element = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Sekarang Jam"), /*#__PURE__*/React.createElement("h2", null, new Date().toLocaleTimeString()));
-
-  // render untuk memunculkan element
-  ReactDOM.render(element, root);
-
-  /** Template literal - cara vanilla js
-   *  semua element yg ada didalam div dirender 
-   *  namun, untuk bisa mengatur agar hanya jam saja yang terupdate cukup sulit
-   *  cara ini tidak efiesien, 
-   *  terutama saat terdapat eventhandler, dan juga
-   *  membuat memori lebih banyak digunakan
-   * 
-   
-  const element = `
-      <div>
-          <h1>Sekarang Jam</h1>
-          ${new Date().toLocaleTimeString()}
-      </div>
-  `;
-    // render untuk memunculkan element
-  root.innerHTML = element **/
+function App() {
+  const nameRef = React.useRef(null);
+  function ketikaSubmit(event) {
+    event.preventDefault();
+    const name = nameRef.current.value;
+    console.log('Name: ', name);
+  }
+  return /*#__PURE__*/React.createElement("form", {
+    onSubmit: ketikaSubmit
+  }, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", null, "Nama: "), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "name",
+    ref: nameRef
+  })), /*#__PURE__*/React.createElement("button", {
+    type: "submit"
+  }, "Kirim"));
 }
 
-tick();
-setInterval(function () {
-  tick();
-}, 1000);
+// render untuk memunculkan element
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/app.js
+++ src/app.js
@@ -1,43 +1,25 @@
 const root = document.querySelector('#root');
 
-console.log('Automatic compile!')
+function App() {
+    const nameRef = React.useRef(null);
 
-function tick() {
-    /** Render ala react
-     *  hanyak akan merender element yg memang butuh berubah seperti jam
-     *  lebih efisien dan menghemat memori
-     **/
-    const element = (
-        <div>
-            <h1>Sekarang Jam</h1>
-            <h2>{new Date().toLocaleTimeString()}</h2>
-        </div>
-    );
+    function ketikaSubmit(event) {
+        event.preventDefault();
+        const name = nameRef.current.value;
 
-    // render untuk memunculkan element
-    ReactDOM.render(element, root);
+        console.log('Name: ', name);
+    }
 
-    
-    /** Template literal - cara vanilla js
-     *  semua element yg ada didalam div dirender 
-     *  namun, untuk bisa mengatur agar hanya jam saja yang terupdate cukup sulit
-     *  cara ini tidak efiesien, 
-     *  terutama saat terdapat eventhandler, dan juga
-     *  membuat memori lebih banyak digunakan
-     * 
-     
-    const element = `
-        <div>
-            <h1>Sekarang Jam</h1>
-            ${new Date().toLocaleTimeString()}
-        </div>
-    `;
-
-    // render untuk memunculkan element
-    root.innerHTML = element **/
+    return (
+        <form onSubmit={ketikaSubmit}>
+            <div>
+                <label>Nama: </label>
+                <input type="text" name="name" ref={nameRef}/>
+            </div>
+            <button type="submit">Kirim</button>
+        </form>
+    )
 }
 
-tick();
-setInterval(function() {
-    tick();
-}, 1000);
+// render untuk memunculkan element
+ReactDOM.render(<App/>, root);
