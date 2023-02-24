d0fc3682378be4a4eca824cc0c95e3655cb95e8b
fidaatag <fidaa2fidaa@gmail.com>
2023-02-24 08:48:35 +0700

    conditional rendering ternary operator

--- public/app.js
+++ public/app.js
@@ -1,18 +1,11 @@
 const root = document.querySelector('#root');
 function App() {
   const [login, setLogin] = React.useState(false);
-  if (login) {
-    return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Yea! Bisa login"), ";", /*#__PURE__*/React.createElement("button", {
-      onClick: function () {
-        setLogin(false);
-      }
-    }, "Log Out"), ";");
-  }
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Login Dulu, Sayton!"), ";", /*#__PURE__*/React.createElement("button", {
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "The Apps"), /*#__PURE__*/React.createElement("button", {
     onClick: function () {
       setLogin(true);
     }
-  }, "Log In"), ";");
+  }, "Log In"), /*#__PURE__*/React.createElement("p", null, login ? /*#__PURE__*/React.createElement("b", null, "Berhasil login") : ''), /*#__PURE__*/React.createElement("p", null, login && /*#__PURE__*/React.createElement("b", null, "Berhasil login")), /*#__PURE__*/React.createElement("p", null, login === true && /*#__PURE__*/React.createElement("b", null, "Berhasil login")), /*#__PURE__*/React.createElement("p", null, login ? '' : /*#__PURE__*/React.createElement("i", null, "Belum login")), /*#__PURE__*/React.createElement("p", null, !login && /*#__PURE__*/React.createElement("i", null, "Belum login")), /*#__PURE__*/React.createElement("p", null, login === false && /*#__PURE__*/React.createElement("i", null, "Belum login")));
 }
 
 // render untuk memunculkan element
--- src/app.js
+++ src/app.js
@@ -3,19 +3,18 @@ const root = document.querySelector('#root');
 function App() {
     const [login, setLogin] = React.useState(false);
 
-    if(login) {
-        return (
-            <>
-            <h1>Yea! Bisa login</h1>;
-            <button onClick ={function() {setLogin(false)}}>Log Out</button>;
-            </>
-        );
-    }
-
     return (
         <>
-            <h1>Login Dulu, Sayton!</h1>;
-            <button onClick ={function() {setLogin(true)}}>Log In</button>;
+            <h1>The Apps</h1>
+            <button onClick ={function() {setLogin(true)}}>Log In</button>
+
+            <p>{login ? <b>Berhasil login</b> : ''}</p>
+            <p>{login && <b>Berhasil login</b>}</p>
+            <p>{login === true && <b>Berhasil login</b>}</p>
+
+            <p>{login ? '' : <i>Belum login</i>}</p>
+            <p>{!login && <i>Belum login</i>}</p>
+            <p>{login === false && <i>Belum login</i>}</p>
         </>
     )
 }
